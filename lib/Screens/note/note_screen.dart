85eda56f67ecd0947373c1104d11d508f72199d7
OUKIL <omaroukil31@gmail.com>
2020-11-02 11:21:41 +0100

    setup for the note body

--- lib/Screens/note/note_screen.dart
+++ lib/Screens/note/note_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:le_savoir_du_nord/Screens/note/components/body.dart';
+import 'package:le_savoir_du_nord/components/Master.dart';
+import 'package:le_savoir_du_nord/Screens/note/components/Body.dart';
 
 class NoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(),
+      body: Master(body: Body()),
     );
   }
 }
--- lib/components/Master.dart
+++ lib/components/Master.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:le_savoir_du_nord/Screens/Login/components/background.dart';
-import 'package:le_savoir_du_nord/Screens/Signup/signup_screen.dart';
 import 'package:le_savoir_du_nord/Screens/Welcome/welcome_screen.dart';
-import 'package:le_savoir_du_nord/components/already_have_an_account_acheck.dart';
-import 'package:le_savoir_du_nord/components/rounded_button.dart';
-import 'package:le_savoir_du_nord/components/rounded_input_field.dart';
-import 'package:le_savoir_du_nord/components/rounded_password_field.dart';
 import 'package:le_savoir_du_nord/constants.dart';
-import 'package:le_savoir_du_nord/Screens/Welcome/components/body.dart';
 
-class Body extends StatefulWidget {
-  Body({Key key}) : super(key: key);
-
-  @override
-  _BodyState createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> {
+class Master extends StatelessWidget {
+  Master({Key key, this.body}) : super(key: key);
   Size size;
   var heightBar = AppBar().preferredSize.height;
-
+  Widget body;
   @override
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
     return Scaffold(
+      body: body,
       appBar: AppBar(
         centerTitle: true,
         title: Image.asset(
