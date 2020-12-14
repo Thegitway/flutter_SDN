85656fc906c8809e1ca7024d13c106d10734c031
OUKIL omar <omaroukil31@gmail.com>
2020-12-14 16:42:35 +0100

    Emsi designe 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Le savoir du nord',
+      title: 'EMSI',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
